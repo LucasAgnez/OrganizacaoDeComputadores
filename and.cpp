4ffa9de1959bde0f42571fe35e0f2831738de88a
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-20 21:06:07 -0300

    Update and rename and.h to and.cpp

---
+++ and.cpp
@@ -0,0 +1,17 @@
+#include "systemc.h"
+
+SC_MODULE(and) {
+	sc_out<sc_uint<32>> A, B;
+	sc_out<sc_uint<32>> CO;
+
+	void do_and() {
+		for(int i=0; i<32; i++){
+			CO[i].write( (A[i].read() && B[i].read()) );
+		}
+	}
+
+	SC_CTOR(and) {
+		SC_METHOD(do_and);
+		sensitive << A << B;
+	}
+};
