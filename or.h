42ee1c8ecd78ecb40bc0e83dbbc1b79b9007a67f
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-22 15:06:53 -0300

    Update or.h

--- or.h
+++ or.h
@@ -1,13 +1,11 @@
 #include "systemc.h"
 
 SC_MODULE(or) {
-	sc_in<sc_uint<32>> A, B;
-	sc_out<sc_uint<32>> S;
+	sc_in<sc_lv<32>> A, B;
+	sc_out<sc_lv<32>> S;
 
 	void do_or() {
-		for(int i=; i<32; i++){
-			S[i].write( (A[i].read() | B[i].read()) );
-		}
+		S.write(A.read() | B.read());
 	}
 
 	SC_CTOR(or) {
