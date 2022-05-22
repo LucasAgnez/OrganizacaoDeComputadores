d0d27ddd931db0531b522ad2356c13e2920c5cf4
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-22 18:21:02 -0300

    Update ULA.h

--- ULA.h
+++ ULA.h
@@ -10,19 +10,58 @@ SC_MODULE(ULA) {
     
 
     void do_ula() {
+      zero.write("0");
       switch(OP.read()) {
         case 0000: {
           S.write(A.read() & B.read());
             break;
         }
+        case 0001: {
+          S.write(A.read() | B.read());
+            break;
+        }
+        case 0010: {
+          S.write(A.read() ^ B.read());
+            break;
+        }
+        case 0011: {
+          S.write(~A.read());
+            break;
+        }
+        case 0100: {
+            if (A == B){
+                zero.write("1");
+            }
+        }
         case 0101: {
           S.write(A.read() + B.read());
             break;
         }
+        case 0110: {
+          S.write(A.read() - B.read());
+            break;
+        }
+        case 0111: {
+            \\leitura
+            break;
+        }
+        case 1000: {
+            \\escrita
+            break;
+        }
+        case 1001: {
+            \\salto inc
+            break;
+        }  
+        case 1010: {
+            \\salto neg
+            break;
+        }
+        case 1011: {
+            \\salto zero
+            break;
+        }
       }
-      zero.write(0);
-        
-   
     }
 
     SC_CTOR(ULA) {
