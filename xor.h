c6d79519f20a5575bbda45ad9eb6d79a826456eb
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-22 15:07:36 -0300

    Update xor.h

--- xor.h
+++ xor.h
@@ -1,8 +1,8 @@
 #include "systemc.h"
 
 SC_MODULE(xor) {
-	sc_in<bool> A, B;
-	sc_out<bool> S;
+	sc_in<sc_lv<32>> A, B;
+	sc_out<sc_lv<32>> S;
 
 	void do_xor() {
 		S.write( (A.read() ^ B.read()) );
