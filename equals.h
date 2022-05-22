635d61bd7478c580d9e37822d7d4b890308dd5b2
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-22 15:05:17 -0300

    Update equals.h

--- equals.h
+++ equals.h
@@ -1,15 +1,15 @@
 #include "systemc.h"
 
 SC_MODULE(equals) {
-	sc_out<sc_uint<32>> A, B;
-	sc_out<sc_uint<32>> S;
+	sc_in<sc_lv<32>> A, B;
+	sc_out<sc_lv<32>> S;
 
 	void do_equals() {
 		if (A == B){
 			S.write(true);
 		}
 		else{
-			  S.write(false);
+			S.write(false);
 		}
 	}
 
