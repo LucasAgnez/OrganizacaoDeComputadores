718f19c3cb23031cda62abd31d4ba51b7c80075c
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-25 09:35:40 -0300

    Implementação de outra parte da PO

--- po.h
+++ po.h
@@ -8,26 +8,82 @@
 SC_MODULE(PO) {
 
     void do_po() {
-      sc_signal<sc_uint<5>> inst_address;
-      sc_signal<sc_uint<24>> inst_data;
-      
-      INST_MEM inst_mem("INST_MEM");
-      inst_mem.inst_address(inst_address);
-      inst_mem.inst_data(inst_data);
-      inst_address.write(0);
-      
-      
-      sc_signal<sc_uint<4>> OP;
-      CTR ctr("CTR");
-      ctr.OP(OP);
-      op.write(inst_data.
-      
+        sc_in<sc_uint<5>> inst_address;
+        sc_out<sc_uint<24>> inst_data;
+
+        INST_MEM inst_mem("INST_MEM");
+        inst_mem.inst_address(inst_address);
+        inst_mem.inst_data(inst_data);
+        inst_address.write(0);
+
+        sc_in<sc_uint<32>> x;
+        sc_in<sc_uint<32>> y;
+        sc_in<sc_uint<4>> ulaop;
+        sc_out<sc_uint<32>> s;
+        sc_out<sc_uint<1>> zero;
+
+        ULA a("ULA");
+        a.A(x);
+        a.OP(ulaop);
+        a.B(y);
+        a.S(s);'
+        a.zero(zero);
+
+        sc_out<sc_uint<1>> regWrite;
+        sc_out<sc_uint<1>> memToReg;
+        sc_out<sc_uint<1>> memWrite;
+        sc_out<sc_uint<1>> memRead;
+        sc_out<sc_uint<1>> aluSrc;
+        sc_in<sc_uint<4>> ctrop;
+
+        CTR ctr("CTR");
+        ctr.OP(ctrop);
+        ctr.regWrite(regWrite);
+        ctr.memToReg(memToReg);
+        ctr.memWrite(memWrite);
+        ctr.memRead(memRead);
+        ctr.aluSrc(aluSrc);
+
+        ctrop.write(inst_data.
+
+        sc_in<sc_uint<5>> data_address;
+        sc_in<sc_uint<1>> mem_write;
+        sc_in<sc_uint<1>> mem_read;
+        sc_in<sc_uint<32>> write_data;
+        sc_out<sc_uint<32>> data_value;
+
+        DATA_MEM dm("DATA MEM");
+        dm.data_address(data_address);
+        dm.mem_write.(mem_write);
+        dm.mem_read.(mem_read);
+        dm.write_data.(write_data);
+        dm.data_value.(data_value);
+
+        
+        sc_in<sc_uint<5>> r1_address;
+        sc_in<sc_uint<5>> r2_address;
+        sc_in<sc_uint<1>> reg_write;
+        sc_in<sc_uint<5>> write_address;
+        sc_in<sc_uint<32>> write_data;
+        sc_out<sc_uint<32>> r1_value;
+        sc_out<sc_uint<32>> r2_value;
+        
+        REG_MEM rm("REG MEM");
+        rm.r1_address(r1_address);
+        rm.r2_address(r2_address);
+        rm.reg_write(reg_write);
+        rm.write_address(write_address);
+        rm.write_data(write_data);
+        rm.r1_value(r1_value);
+        rm.r2_value(r2_value);
       
       
     }
 
-    SC_CTOR(ULA) {
+    SC_CTOR(PO) {
         SC_METHOD(do_po);
         sensitive << A << B << OP;
     }
 };
+
+
