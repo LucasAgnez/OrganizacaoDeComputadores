94c42324900c62cd2c41a92da0ac476dbfbea133
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2022-05-25 09:52:35 -0300

    Adiciona clock ao testbench (e arquivo de simulação de ondas)

--- testbench.cpp
+++ testbench.cpp
@@ -1,20 +1,52 @@
 #include "systemc.h"
-#include "and.h"
+#include "ULA.h"
+#include "ctr.h"
 
 int sc_main (int argc, char* argv[]) {
-    sc_signal<sc_lv<32>>  x, y;
-    sc_signal<sc_lv<32>>  s;
-
-    AND a("a");
+  	sc_signal<bool> clock;
+  
+  	sc_signal<sc_uint<32>> x, y;
+  	sc_signal<sc_uint<4>> op;
+  	sc_signal<sc_uint<1>> z;
+    sc_signal<sc_uint<32>>  s;
+  
+    ULA a("a");
     a.A(x);
     a.B(y);
     a.S(s);
-    x.write("1011");
-    y.write("1110");
+  	a.OP(op);
+  	a.zero(z);
+  	a.clock(clock);
   
-    sc_start();
+  	// Open VCD file
+    sc_trace_file *wf = sc_create_vcd_trace_file("processor");
+    // Dump the desired signals
+    sc_trace(wf, clock, "clock");
+  	sc_trace(wf, x, "x");
+    sc_trace(wf, y, "y");
+  	sc_trace(wf, s, "s");
   
-    cout << s.read()<<endl;
+  	int j, k;
+  	j = 5;
+  	k = 6;
+  	op.write(4);
+  	x.write(j);
+  	y.write(k);
+  	for(int i=0; i<5; i++) {
+      clock = 0; 
+      sc_start(1, SC_NS);
+      clock = 1; 
+      x.write(j++);
+      y.write(k++);
+      sc_start(1, SC_NS);
+      cout << s.read()<<endl;
+    }
+    
+  	
+  	
+    //sc_start(1, SC_SEC);
+  
+    
   
   return 0;
 }
