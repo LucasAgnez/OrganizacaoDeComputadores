3a49dc2d53dbf697605295d715a5193d96e7a3fe
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-29 20:53:40 -0300

    Update reg_mem.h

--- reg_mem.h
+++ reg_mem.h
@@ -8,12 +8,12 @@ SC_MODULE(REG_MEM) {
 	
 	sc_in<sc_uint<1>> reg_write;
 	sc_in<sc_uint<5>> write_address;
-	sc_in<sc_uint<32>> write_data;
+	sc_in<sc_uint<5>> write_data;
 	
-	sc_out<sc_uint<32>> r1_value;
-	sc_out<sc_uint<32>> r2_value;
+	sc_out<sc_uint<5>> r1_value;
+	sc_out<sc_uint<5>> r2_value;
 
-	sc_uint<32> registers[32];
+	sc_uint<5> registers[32];
 	
 
 
