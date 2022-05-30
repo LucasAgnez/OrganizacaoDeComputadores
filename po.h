31b3003b75b25ab9fe170bbcd08523661b902ff7
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2022-05-29 21:15:13 -0300

    Atualiza po

--- po.h
+++ po.h
@@ -51,8 +51,8 @@ SC_MODULE(PO) {
   }
   
   void alu_ini() {
-    alu->A(alu_x);
     alu->OP(alu_op);
+    alu->A(alu_x);
     alu->B(alu_y);
     alu->S(alu_s);
     alu->zero(alu_zero);
@@ -89,55 +89,199 @@ SC_MODULE(PO) {
     rm->clock(clock);
   }
   
+  void decode_instruction(sc_uint<24> instruction, sc_uint<4>* opcode, sc_uint<5>* rt, sc_uint<5>* rs, sc_uint<5>* desloc_rd) {
+  	
+    //Bits [23-20] (opcode) 
+    *opcode = instruction >> 20;
+    //Bits [19-15]
+    *rt = inst_mem_data.read() >> 15;
+    //Bits [14-10]
+    *rs = inst_mem_data.read() >> 10;
+    //Bits [9-5]
+    *desloc_rd = inst_mem_data.read() >> 5;
+    
+    switch(*opcode) {
+        case 7: {
+          //Bits [19-15]
+          *rt = inst_mem_data.read() >> 15;
+          //Bits [14-10]
+          *rs = inst_mem_data.read() >> 10;
+          //Bits [9-5]
+          *desloc_rd = inst_mem_data.read() >> 5;
+          break;
+        }
+        case 8: {
+          //Bits [19-15]
+          *rt = inst_mem_data.read() >> 15;
+          //Bits [14-10]
+          *rs = inst_mem_data.read() >> 10;
+          //Bits [9-5]
+          *desloc_rd = inst_mem_data.read() >> 5;
+          break;
+        }
+        case 9: {
+          //Bits [9-5]
+          *desloc_rd = inst_mem_data.read() >> 5;
+          break;
+        }
+        case 10: {
+          //Bits [9-5]
+          *desloc_rd = inst_mem_data.read() >> 5;
+          break;
+        }
+        case 11: {
+          //Bits [9-5]
+          *desloc_rd = inst_mem_data.read() >> 5;
+          break;
+        } 
+        default: {
+          //Bits [19-15]
+          *rt = inst_mem_data.read() >> 15;
+          //Bits [14-10]
+          *rs = inst_mem_data.read() >> 10;
+          //Bits [9-5]
+          *desloc_rd = inst_mem_data.read() >> 5;
+          break;
+        }
+      } 
+  
+  }
+  
+  
+  typedef struct buffer1 {
+  	sc_uint<24> instruction;
+    sc_uint<5> next_inst_addres;
+  } BufferIfId;
+  
+  typedef struct buffer2 {
+  	sc_uint<5> next_inst_addres;
+    sc_uint<5> rt;
+    sc_uint<5> rd;
+    sc_uint<5> opCode;
+    sc_uint<5> desloc; 
+    sc_uint<5> reg_mem_r1_v;
+    sc_uint<5> reg_mem_r2_v;
+    sc_uint<1> = ctr_regWrite;
+    sc_uint<1> = ctr_memToReg;
+    sc_uint<1> = ctr_memWrite;
+    sc_uint<1> = ctr_memRead;
+    sc_uint<1> = ctr_aluSrc;
+    
+  } BufferIdEx;
+  
+  typedef struct buffer3 {
+  	sc_uint<24> instruction;
+    sc_uint<5> branch_inst_addres;
+  } BufferExMem;
   
   
     void do_po() {
-      //Pega os primeiros 4 bits de instrução (opcode) e manda para o Controle
-      sc_uint<4> opcode = inst_mem_data.read() >> 20;
-      ctr_ctrop.write(opcode);
       
+      //INSTRUCTION FETCH
+      BufferIfId bff1;
+      
+      
+      
+      //BUFFER IF/ID
+      //Instruction Memory
+      beff1.instruction = inst_mem_data.read();
+      beff1.next_inst_addres = inst_mem_adress.read() + 1;
+     
+      
+      
+      
+      //INSTRUCTION DECODE
+      BufferIfId bff2;
+     
+      //Divide bits da instrução
+      sc_uint<4> opcode;
+      sc_uint<5> rg_r1_address;
+      sc_uint<5> rg_r2_address;
+      sc_uint<5> desloc;
+      sc_uint<5> rd;
+      decode_instruction(bff1.instruction, &opcode, &rg_r1_address, &rg_r2_address, 
+                         &desloc, &rd);
       cout << "opcode: " << opcode << endl;
       cout << "clock: " << clock << endl;
       
       
-      switch opcode {
-        case 7: {
-          sc_uint<5> rt = inst_mem_data.read() >> 15;
-          sc_uint<5> rs = inst_mem_data.read() >> 10;
-          sc_uint<10> desloc = inst_mem_data.read() >> 5;
-          break;
-        }
-        case 8: {
-          sc_uint<5> rt = inst_mem_data.read() >> 15;
-          sc_uint<5> rs = inst_mem_data.read() >> 10;
-          sc_uint<10> desloc = inst_mem_data.read() >> 5;
-          break;
-        }
-        case 9: {
-          sc_uint<20> addres = inst_mem_data.read() >> 4;
-          break;
-        }
-        case 10: {
-          sc_uint<20> addres = inst_mem_data.read() >> 4;
+      //CONTROL
+      ctr_ctrop.write(opcode);
+      
+      
+      //REGISTERS BANK
+      // Seta entradas de controle do banco de registradores
+      reg_mem_reg_write.write(bff4.ctr_regWrite);
+      
+      reg_mem_write_address.write(bff4.rt);
+      reg_mem_write_data.write(data_mem_data_value.read());
+      
+      reg_mem_r1_address.write(rg_r1_address);
+      reg_mem_r2_address.write(rg_r2_address);
+      
+      
+      
+      //Extende deslocamento de 10bits para 32bits
+//       sc_uint<1> desloc_msb = desloc >> 9;
+      
+//       sc_uint<32> desloc_extended = 0;
+//       sc_uint<32> se1 = 4294966272;
 
-          break;
-        }
-        case 11: {
-          sc_uint<20> addres = inst_mem_data.read() >> 4;
-          break;
-        } 
-        default: {
-          sc_uint<5> rt = inst_mem_data.read() >> 15;
-          sc_uint<5> rs = inst_mem_data.read() >> 10;
-          sc_uint<5> rd = inst_mem_data.read() >> 5;
-          break;
-        }
-      } 
+//       if (desloc_msb == 1){
+//         desloc_extended = se1 + desloc;
+//       } else {
+//         desloc_extended = desloc;
+//       }
+      
+      
+      
+      //BUFFER ID/EX
+      bff2.opCode = opcode;
+      
+      bff2.ctr_regWrite = ctr_regWrite.read();
+      bff2.ctr_memToReg = ctr_memToReg.read();
+      bff2.ctr_memWrite = ctr_memWrite.read();
+      bff2.ctr_memRead = ctr_memRead.read();
+      bff2.ctr_aluSrc = ctr_aluSrc.read();
+      
+      bff2.next_inst_addres = bff1.next_inst_addres;
+      
+      bff2.reg_mem_r1_v = r1_value.read();
+      bff2.reg_mem_r2_v = r2_value.read();
+      
+      bff2.desloc = desloc;
+      
+      bff2.rt = rt;
+      bff2.rd = rd;
+      
+      
+      
+      //EXECUTION
+      
+      //ALU
+      alu_op.write(bff2.opCode);
+      
+      alu_x.write(bff2.reg_mem_r1_v);
+      
+      if(ctr_aluSrc == 1) {
+        alu_y.write(bff2.desloc);
+      }
+      else {
+      	alu_y.write(bff2.reg_mem_r1_v);
+      }
+      
+      
+      //BUFFER EX/MEM
+      bff3.branch_inst_addres =  bff2.next_inst_addres + bff2.desloc;
+      
+
       
       
       
       
-     
+      
+      
+      
       
       //-------DATA MEMORY
       // Seta entradas de controle do banco de dados
@@ -146,23 +290,15 @@ SC_MODULE(PO) {
       
       cout << "ctr_memRead: " << ctr_memRead.read() << endl;
       
-      //Pega bits 15-11 da palavra de instrução(rs, tipo I) 
-      sc_uint<5> inst_dm_address = inst_mem_data.read() >> 10;
-      data_mem_data_address.write(inst_dm_address);
+      data_mem_data_address.write(bff1.rs);
       cout << "inst_dm_address: " << inst_dm_address << endl;
       
       
+    
+        
+      BufferExMem bff3;
       
-      //-------REGISTERS BANK
-      // Seta entradas de controle do banco de registradores
-      reg_mem_reg_write.write(ctr_regWrite.read());
       
-      //Pega bits 20-16 da palavra de instrução (rt, tipo I)
-      sc_uint<5> inst_dr_write_address = inst_mem_data.read() >> 15;
-      reg_mem_write_address.write(inst_dr_write_address);
-      cout << "inst_dr_write_address: " << inst_dr_write_address << endl;
-      
-      reg_mem_write_data.write(data_mem_data_value.read());
       
       
       //TESTE
