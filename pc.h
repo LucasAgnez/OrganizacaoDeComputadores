25cc06c9530920e019433bca85b4ba6fb12b2e8d
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-05-30 20:04:38 -0300

    Conserta bugs da PC

--- pc.h
+++ pc.h
@@ -8,13 +8,13 @@ SC_MODULE(PC) {
   	//sc_in<sc_uint<1>> enable;
  	sc_out<sc_uint<5>> next_inst_address;
   
-  	void do_pc
+  	void do_pc(){
 	//	if(enable){
 			next_inst_address.write(inst_address);
 	//	}
 	}
 
-	SC_CTOR(pc){
+	SC_CTOR(PC){
 		SC_METHOD(do_pc);
 			sensitive << clock.neg();
 			//sensitive << endereco;
