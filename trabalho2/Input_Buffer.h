b3d234d254191dbdea9383da6e5a258404208aea
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-07-11 13:14:40 -0300

    Corrige problemas de sincronia

--- trabalho2/Input_Buffer.h
+++ trabalho2/Input_Buffer.h
@@ -16,10 +16,6 @@ SC_MODULE(Input_Buffer) {
   bool full = false;
   
   void exec() {
-    std::cout << "IB Data in: " << data_in.read() << std::endl;
-    std::cout << "IB wr: " << wr.read() << std::endl;
-    std::cout << "IB rd: " << rd.read() << std::endl;
-    std::cout << "Full: " << full << std::endl;
     if(full){
       wok.write(0);
       rok.write(1);
@@ -36,7 +32,6 @@ SC_MODULE(Input_Buffer) {
         full = 1;
       }
     }
-    std::cout << "IB Data out: " << data_out.read() << std::endl;
   }
 
   SC_CTOR(Input_Buffer) {
