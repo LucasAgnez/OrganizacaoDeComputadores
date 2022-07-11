d93287d1bdcca8865a1082eb9d1efb32a874545e
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-07-11 13:15:44 -0300

    Corrige problemas de sincronia

--- trabalho2/Input_Ctr.h
+++ trabalho2/Input_Ctr.h
@@ -23,17 +23,12 @@ SC_MODULE(Input_Ctr) {
     left_right = data_in.read() >> 29;
     Y = data_in.read() >> 27;
     up_down = data_in.read() >> 26;
-    std::cout << "IC x: " << X << std::endl;
-    std::cout << "IC y: " << Y << std::endl;
-    std::cout << "IC left_right: " << left_right << std::endl;
-    std::cout << "IC up_down: " << up_down << std::endl;
     if(X == 0 && Y == 0){
       n_req.write(0);
       s_req.write(0);
       e_req.write(0);
       w_req.write(0);
       l_req.write(1);
-      std::cout << "AAAAAA" << std::endl;
     }
     else if(X>0){
       X--;
@@ -60,7 +55,6 @@ SC_MODULE(Input_Ctr) {
         e_req.write(0);
         w_req.write(0);
         l_req.write(0);
-        std::cout << "AAAAAA" << std::endl;
       }
       else if(up_down == 1){
         n_req.write(1);
@@ -71,14 +65,7 @@ SC_MODULE(Input_Ctr) {
       }
     }
     if(rok.read() == 1){
-      std::cout << "IC l_req: " << l_req.read() << std::endl;
-      std::cout << "IC Data in: " << data_in.read() << std::endl;
-      std::cout << "IC Data out: " << data_out.read() << std::endl;
-      std::cout << "IC n_req: " << n_req.read() << std::endl;
-      std::cout << "IC s_req: " << s_req.read() << std::endl;
-      std::cout << "IC up_down: " << up_down << std::endl;
-      std::cout << "IC e_req: " << e_req.read() << std::endl;
-      std::cout << "IC w_req: " << w_req.read() << std::endl;
+
 
       sc_uint<6> new_header;
       new_header = (X<<4) + (left_right<<3) + (Y<<1) + up_down;
@@ -86,8 +73,6 @@ SC_MODULE(Input_Ctr) {
       new_data = new_data >> 6;
       new_data = new_data + (new_header << 26);
       data_out.write(new_data);
-      std::cout << "IC new data: " << new_data << std::endl;
-
     }
   }
 
